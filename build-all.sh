5b49a290383f137bd095c87d36008ee06a992d4f
Doug Anson <doug.anson@arm.com>
2026-06-26 12:47:18 -0500

    optional param passing

--- build-all.sh
+++ build-all.sh
@@ -3,6 +3,6 @@
 set -x
 
 chmod 755 *.sh
-./build-orin-super-nano.sh
-./build-thor.sh
-./build-orinnx.sh
+./build-orin-super-nano.sh $*
+./build-thor.sh $*
+./build-orinnx.sh $*
--- build-orin-super-nano.sh
+++ build-orin-super-nano.sh
@@ -4,4 +4,4 @@ set -x
 
 rm -rf oe4t*
 chmod 755 *.sh 
-TARGET_PLATFORM=orin-super-nano ./build_oe4t_jetson_multi_platform.sh
+TARGET_PLATFORM=orin-super-nano ./build_oe4t_jetson_multi_platform.sh $*
